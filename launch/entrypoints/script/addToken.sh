a7fd8db73c67cf3312774ae29c8ffa0306bfc775
sampwhite <swhite@gyassa.com>
2019-01-29 15:18:57 -0600

    [tagScript] Fixed up script to add tokens

--- launch/entrypoints/script/addToken.sh
+++ launch/entrypoints/script/addToken.sh
@@ -2,3 +2,7 @@
 # Adds a token that can be used to act as particular user or to login as that user.
 # Command line is
 # addToken <username> <tokenId> <tokenPassword>
+SCRIPT=$(readlink -f "$0")
+PATH_TO_DIR=`dirname "$SCRIPT"`
+
+$PATH_TO_DIR/execScript.sh script.AddToken $@
