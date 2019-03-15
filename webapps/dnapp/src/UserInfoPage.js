32f216a1ab654c355863d4eb6a42bffb19e5fa3b
sampwhite <swhite@gyassa.com>
2019-03-15 09:57:05 -0500

    [tagReact] Fixed layout issue for user info

--- webapps/dnapp/src/UserInfoPage.js
+++ webapps/dnapp/src/UserInfoPage.js
@@ -14,6 +14,7 @@ class UserInfoPage extends Component {
     return (
       <div className="presentBox">
         <UserRegistrationInfo/>
+        <br/>
         <UserLoginSources/>
       </div>
     )
